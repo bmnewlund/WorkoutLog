0a35bb88df1fce96de41bb8abe05e8d191d64631
Brett Newlund <bmnewlund@gmail.com>
2017-09-02 07:59:09 -0400

    Added app.js, index.html, and app.js

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express();
 
+app.use('/api/test', function(req, res){
+	res.send("Hello World");
+});
+
 app.listen(3000, function(){
 	console.log("app is open on 3000!");
 })
