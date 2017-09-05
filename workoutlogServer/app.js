c0d8fb8044c8bfa7c9fe1dc341c1d6f48eee6a5a
Brett Newlund <bmnewlund@gmail.com>
2017-09-05 09:19:25 -0400

    Added changes to app.js

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -2,13 +2,68 @@ var express = require('express');
 
 var app = express();
 
+var bodyParser=require('body-parser');
+
 app.use(require('./middleware/headers'));
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
+
 app.listen(3000, function(){
 	console.log("app is open on 3000!");
-})
+});
+
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'yourpassword', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+
+sequelize.authenticate().then(
+	function() {
+		console.log('connected to workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+
+// Data Model
+var User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
+});
+
+User.sync();
+
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req,res) {
+	var username = req.body.user.username;
+	var pass = req.body.user.password;
+	
+	User.create({
+		username: username,
+		passwordhash: ""
+	}).then(
+		function createSuccess(user){
+			res.json({
+				user: user,
+				message: 'create'
+			})
+		
+		},
+		function createError(err){
+			res.send(500, err.message);
+		}
+	);
+	// Need to create a user object and use sequelize to put that user into
+	// our database
+});
+
+// User.sync({force: true});
 
