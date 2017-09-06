d2c3a192954fd8d8f97f956dca40bc4e6717eecf
Brett Newlund <bmnewlund@gmail.com>
2017-09-06 09:26:06 -0400

    Added auth.js and user folder

---
+++ workoutlogClient/user/auth.js
@@ -0,0 +1,45 @@
+$(function(){
+	$.extend(WorkoutLog, {
+		//signup method
+		signup: function(){
+			//username & password variables.
+			var username = $("#su_username").val();
+			var password = $("#su_password").val();
+			//user object
+			var user = {
+				user: {
+					username: username,
+					password: password
+				}
+			};
+
+			//signup post
+			var signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JASON.stringify(user),
+				contentType: "application/json"
+			});
+
+			//signup done/fail
+			signup.done(function(data) {
+				if(data.sessionToken) {
+					WorkoutLog.setAuthHeader(data.sessionToken);
+
+				}
+				$("signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+
+			}).fail(function() {
+				$("su_error").text("There was in issue with signup").show();
+			});
+		}
+
+		//login method
+
+		//loginout method
+	});
+	//blind events
+	$("#signup").on("click", WorkoutLog.signup);
+});
